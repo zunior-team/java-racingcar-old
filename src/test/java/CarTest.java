2505c602b191df28b3700ecb37c7703eb016317c
최준우 <junwoochoi@zuminternet.com>
2020-01-14 10:26:22 +0900

    test : 이동을 위한 자동차 테스트 작성

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,12 +1,14 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Car 객체 단위 테스트")
 class CarTest {
-    
+
     @Test
     @DisplayName("Car 객체를 생성한다.")
     void createCarTest() {
@@ -17,5 +19,19 @@ class CarTest {
         assertThat(car).isNotNull();
     }
 
+    @ParameterizedTest
+    @DisplayName("Car 는 주어진 조건이 true일 때 이동한다")
+    //given
+    @CsvSource({"true,1", "false,0"})
+    void testMove(boolean movingCondition, int result) {
+        //when
+        Car car = Car.newInstance();
+        int positionBeforeMove = car.currentPositon();
+        car.move(() -> movingCondition);
+        int positionAfterMove = car.currentPositon();
+
+        //then
+        assertThat(positionAfterMove - positionBeforeMove).isEqualTo(result);
+    }
 
 }
