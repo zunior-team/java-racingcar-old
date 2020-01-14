56ab71cec7dabb024479783f344a0b61334eb868
nokchax <nokchax@gmail.com>
2020-01-14 20:27:40 +0900

    comment : about constructor test

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 public class CarsTest {
     @Test
     public void CarsTest() {
+        // is constructor test required??
         Cars cars = new Cars(10);
 
     }
