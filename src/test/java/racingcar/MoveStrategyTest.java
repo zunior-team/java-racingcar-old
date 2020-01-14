4666370295d4e4e78f94c0b4218d17ea22e19ca6
nokchax <nokchax@gmail.com>
2020-01-14 20:05:40 +0900

    feat : random move strategy test and implements

---
+++ src/test/java/racingcar/MoveStrategyTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static racingcar.RandomMoveStrategy.MOVE_CRITERIA;
+
+@ExtendWith(MockitoExtension.class)
+public class MoveStrategyTest {
+    @Mock
+    private Random random;
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3, 4, 5, 9})
+    public void moveStrategyTest(int randomNumber) {
+        RandomMoveStrategy randomMoveStrategy = new RandomMoveStrategy(random);
+
+        given(random.nextInt(anyInt())).willReturn(randomNumber);
+
+        assertThat(randomMoveStrategy.isMovable()).isEqualTo(randomNumber >= MOVE_CRITERIA);
+    }
+}
