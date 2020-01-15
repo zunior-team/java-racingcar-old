6a94b5104fc655c61ea6eeeca12405abe61d146b
최준우 <junwoochoi@zuminternet.com>
2020-01-15 09:34:47 +0900

    refactor : test 파일들 package 분리

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -1,3 +1,5 @@
+package car;
+
 import car.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/game/RacingGameTest.java
+++ src/test/java/game/RacingGameTest.java
@@ -1,3 +1,5 @@
+package game;
+
 import game.RacingGame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
