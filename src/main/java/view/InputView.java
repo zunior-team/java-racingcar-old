dec48035d0dd0ac391e27dd45d146015a31b15f9
최준우 <junwoochoi@zuminternet.com>
2020-01-21 09:57:44 +0900

    refactor: 입력받은 이름을 CommaSplitter로 나누도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,7 @@ package view;
 import game.RacingGame;
 import random.RandomGenerator;
 import strategy.RandomMovingStrategy;
+import util.CommaSplitter;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -11,14 +12,14 @@ public class InputView {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        final int numberOfCars = Integer.parseInt(scanner.nextLine());
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        final String carNamesInOneLine = scanner.nextLine();
         System.out.println("시도할 회수는 몇 회 인가요?");
         final int countOfTurns = Integer.parseInt(scanner.nextLine());
 
         final Random generate = RandomGenerator.generate();
         final RandomMovingStrategy movingStrategy = new RandomMovingStrategy(generate);
-        final RacingGame racingGame = RacingGame.newInstance(numberOfCars, countOfTurns, movingStrategy);
+        final RacingGame racingGame = RacingGame.newInstance(CommaSplitter.split(carNamesInOneLine), countOfTurns, movingStrategy);
 
         ResultView.print(racingGame.proceedAllAndGetResult());
     }
